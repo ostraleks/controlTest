f65318b8fab770bda702b69d1be12614aa5b2613
aleksndr <ostraleks@gmail.com>
2022-09-19 11:23:52 +0500

    add programm

---
+++ programm/Program.cs
@@ -0,0 +1,34 @@
+﻿string[] initialArray = {"dfghg", "er?", "34", "df#56", ",", "end", "vcbgf", "456", "<>(", "цы"};
+
+int numberOfStrings(string[] firstArray)
+{
+    int result = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {    
+        if (firstArray[i].Length <= 3)
+        
+            result ++;
+    }
+    return result;
+}
+
+string[] stringsArray(string[] secondArray, int dimension)
+{
+    string[] outputArray = new string[dimension];
+    dimension = 0;
+    for (int i = 0; i < secondArray.Length; i++)
+    {
+    if (secondArray[i].Length <= 3)
+        {
+        outputArray[dimension] = secondArray[i];
+        dimension ++;
+        }
+    }
+    return outputArray;
+}
+
+int number = numberOfStrings(initialArray);
+
+string [] resultArray = stringsArray(initialArray, number);
+
+Console.WriteLine($"Результат: [ {String.Join("; ", resultArray)} ]");
